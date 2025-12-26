48f04bf4ab7fbea5eab7ab8bf086057cc30a5a42
robinsgupta09-maker <robinsgupta09@gmail.com>
2025-12-26 21:14:11 +0530

    Create hand_tracking_module.py

---
+++ hand_tracking_module.py
@@ -0,0 +1,38 @@
+import cv2
+import mediapipe as mp
+import math
+
+class HandDetector:
+    def __init__(self, mode=False, max_hands=1, detection_con=0.7, track_con=0.5):
+        self.mp_hands = mp.solutions.hands
+        self.hands = self.mp_hands.Hands(mode, max_hands, 1, detection_con, track_con)
+        self.mp_draw = mp.solutions.drawing_utils
+        self.tip_ids = [4, 8, 12, 16, 20]
+
+    def find_hands(self, img, draw=True):
+        img_rgb = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+        self.results = self.hands.process(img_rgb)
+        if self.results.multi_hand_landmarks:
+            for hand_lms in self.results.multi_hand_landmarks:
+                if draw:
+                    self.mp_draw.draw_landmarks(img, hand_lms, self.mp_hands.HAND_CONNECTIONS)
+        return img
+
+    def find_position(self, img, hand_no=0, draw=True):
+        self.lm_list = []
+        if self.results.multi_hand_landmarks:
+            my_hand = self.results.multi_hand_landmarks[hand_no]
+            for id, lm in enumerate(my_hand.landmark):
+                h, w, c = img.shape
+                cx, cy = int(lm.x * w), int(lm.y * h)
+                self.lm_list.append([id, cx, cy])
+                if draw:
+                    cv2.circle(img, (cx, cy), 5, (255, 0, 0), cv2.FILLED)
+        return self.lm_list
+
+    def get_distance(self, p1, p2, img, draw=True):
+        x1, y1 = self.lm_list[p1][1], self.lm_list[p1][2]
+        x2, y2 = self.lm_list[p2][1], self.lm_list[p2][2]
+        cx, cy = (x1 + x2) // 2, (y1 + y2) // 2
+        length = math.hypot(x2 - x1, y2 - y1)
+        return length, img, [x1, y1, x2, y2, cx, cy]
